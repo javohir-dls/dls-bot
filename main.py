b5e7bebeae05b28646743669f0aa0cd97b84db30
javohir-dls <chorikichinov21@gmail.com>
2026-06-26 18:09:39 +0500

    Initialize Telegram bot with logging
    Added logging and Telegram bot initialization code.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import logging
+from telegram.ext import ApplicationBuilder
+
+# Tokeningizni shu yerga yozing
+TOKEN = "8877479664:AAFLQjuDtLcKvrWofQt2LftuGJMabV46hig"
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    print("Bot ishga tushdi!")
+    application.run_polling()
 
