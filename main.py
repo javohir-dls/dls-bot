ad3bbc0fa09f709e2e714b389beb39a4ab99dfbf
javohir-dls <chorikichinov21@gmail.com>
2026-06-26 19:32:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import logging
+from telegram.ext import ApplicationBuilder
 from flask import Flask
 import threading
 
+# Flask qismi (Render uchun kerak)
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     return "Bot ishlayapti!"
 
-def run():
+def run_flask():
     app.run(host='0.0.0.0', port=10000)
 
-threading.Thread(target=run).start()
+# Tokeningiz
+TOKEN = "8877479664:AAFLQjuDtLcKvrWofQt2LftuGJMabV46hig"
+
+if __name__ == '__main__':
+    # Flaskni fonda ishga tushirish
+    threading.Thread(target=run_flask).start()
+    
+    # Botingizni ishga tushirish
+    application = ApplicationBuilder().token(TOKEN).build()
+    print("Bot ishga tushdi!")
+    application.run_polling()
+
